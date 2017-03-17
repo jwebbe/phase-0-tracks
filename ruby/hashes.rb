ebeef67485b2d4c49d6eaa3cea6e178c1c95ef29
Jay Webbe <jwebbe@me.com>
2017-03-16 23:55:37 -0400

    Initialized Hashes 5.2

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+#  --- 5.2 Arrays & Hashes ---
+
+# Write a program that will allow an interior designer to enter the details of a given client: the client's name, age, number of children, decor theme, and so on (choosing a mix of string, integer, and boolean data).
+
+# Create a Hash with the list of items to ask from the client.
+clients_info = {
+  name: " ",
+  age: [],
+  number_of_children: [],
+  decor_theme: " ",
+  number_of_pets: [],
+  budget: [],
+  hobbies: " ",
+  allergies: " "
+}
+
+# Prompt Interior Designer to input each data for each key for their pair values.
