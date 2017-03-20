96a8de4f1b971ec5f22fb9747d988ffcb2ff4984
Jay Webbe <jwebbe@me.com>
2017-03-20 02:32:18 -0400

    Re complete alias manager release 1 -2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,64 +6,94 @@
 # Provide a user interface that lets a user enter a name and return a fake name back.
 # User a data structure to store the fake names entered and iterate through the data structure to print all the data that was entered.
 
-
-# Create an empty Hash to store our names.
-# Set Alias names to false to run for our loop later on.
-
-former_name = nil
-secret_alias = {}
-
-
-# Create a loop while condition is false to attain Agent's Full Name.
-until former_name == "quit"
-
-  puts "Please enter your Former Name or type 'quit' to end."
-  former_name = gets.chomp
+# Create a Method that Swaps names
+# Names will split the full string of names into multiple ones
+# We then reverse the names and keep them stored
+# We then join the string together.
+def swapping_names(name)
+  names = name.split('')
+  names.reverse!
+  names.join('')
 end
 
-# Store that name into our variable and change any text to lowercase.
-agents_name = former_name.downcase.split('')
+# Create a method to swap the vowels.
+# Declare a hash for all the vowels and the next changing items to be accessed of their values.
+def swapping_vowels(vowel_char)
+  vowels = { 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a' }
 
-# Create a Method to suffle through Agent's Names with one parameter(Agent's Name)
-def shuffle(agents_name)
-  # Count the length of the Agent's name and store that in a variable.
-  agents_name_length = agents_name.length
-
-  index = 0
-  # Create a loop to start at 0 and run through the length of letters and change each one to the next letter
-  while index <= agents_name_length
-    agents_name[index] = agents_name[index].next
-
-  index += 1
+# We take our arguement and run the iteration and create our if/els/els statement.
+# we use .map to store the next change of our letter and if the vowels are included printer the letter of the vowels otherwise print them downcase and switch them to uppercase.
+  vowel_char.map! do |letter|
+    if vowels.include?(letter)
+      vowels[letter]
+    elsif vowels.include?(letter.downcase)
+      vowels[letter.downcase].upcase
+    else
+      letter
+    end
   end
 end
 
+# We take our arguement and run the iteration and create our if/els/els statement.
+# we use .map to store the next change of our letter and if the vowels are included printer the letter of the vowels otherwise print them downcase and switch them to uppercase.
+def swapping_consonents(consonant_char)
+  consonents = { 'b' => 'c', 'c' => 'd', 'd' => 'f', 'f' => 'g', 'g' => 'h', 'h' => 'j', 'j' => 'k', 'k' => 'l', 'l' => 'm', 'm' => 'n', 'n' => 'p', 'p' => 'q', 'q' => 's', 's' => 't', 't' => 'v', 'v' => 'w', 'w' => 'x', 'x' => 'y', 'y' => 'z', 'z' => 'b' }
 
-
-
-
-
-alias_names = shuffle_of_names[agents_name]
-
-flip_names = alias_names.split('').reverse
-
-puts names_reversed = flip_names.join('')
-
-condition_name = names_reversed.gsub
-
-puts condition_name.upcase
-
-puts true_name = condition_name.split.map
-
-secret_alias[secret_name] = true_name
-
-  if secret_name == "complete"
-    alias_names = true
-  else
-    alias_names = false
+  consonant_char.map! do |letter|
+    if consonents.include?(letter)
+      consonents[letter]
+    elsif consonents.include?(letter.downcase)
+      consonents[letter.downcase].upcase
+    else
+      letter
+    end
   end
-
-secret_alias.each do |secret_name, true_name|
-  puts "The secret name of #{secret_name} is formally known as #{true_name}."
 end
+
+# Create a Method that takes the swap names method and calls the secret name and store that into a variable.
+# take that reverse name and split the string
+# take the swapping vowels method and call the variable for splitting the reverse names.
+# take the swapping consonents methods with swapping vowels that were reversed and split and join them again to be printed.
+def secret_alias_maker(secret_name)
+  reverse_name = swapping_names(secret_name)
+  char = reverse_name.split('')
+  char_swapped_vowels = swapping_vowels(char)
+  added_to_array = swapping_consonents(char_swapped_vowels)
+  added_to_array.join('')
+end
+
+# create an empty string for the name and a database for the names to be stored in.
+# iterate through the name unless the user says to quit
+# We get that name and call our method maker and place in the secret name and print it back. then take that and store it in our data base and return the name backwards.
+# re print the database to confirm that it was printed.
+  name = ''
+  database = {}
+
+until name == "quit"
+
+  puts "Please enter your full name:"
+  name = gets.chomp
+    unless name == "quit"
+      return_alias = secret_alias_maker(secret_name)
+      puts return_alias
+      database[name] = return_alias
+    end
+end
+
+puts database
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
