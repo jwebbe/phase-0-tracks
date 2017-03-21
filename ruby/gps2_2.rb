26615d3ddbfa73a39f05f50e18ef648acebfde80
Jay Webbe <jwebbe@me.com>
2017-03-21 00:42:45 -0400

    complete gps 2.2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -9,46 +9,78 @@
 # output: a hash
 
 # Method to add an item to a list
-# input: list(HASH), item name(KEY), quantity(VALUE)
+# input: list, item name, and optional quantity
 # steps:
   # add item name as a key to the list and set quantity
 # output: updated hash list
 
 # Method to remove an item from the list
-# input: list(HASH), item name(KEY), quanity(VALUE)
+# input: list, item name
 # steps:
-    # delete key from the list and qty.
+  # IF the key exists in the hash
+    # remove key, value pair in the hash
 # output: updated hash list
 
 # Method to update the quantity of an item
-# input: list(HASH), item name(KEY), quanity(VALUE)
-# steps:  update the list hash value
-# output: updated hash list with new qty.
+# input: list, item name, new quantity
+# steps:
+  # IF the key exists in the hash
+    # update the value of that key to the new quantity
+# output: updated hash list
 
 # Method to print a list and make it look pretty
-# input: list(HASH), item name(KEY), quanity(VALUE)
-# steps: print the list of items keys and values.
-# output: print hash list with complete items.
+# input: list
+# steps:
+  # FOR EACH key, value pair of the hash
+    # print out those values
+# output: nil
 
 def create_list(items)
-  items = items.split('')
+
+  grocery_items = items.split(" ")
   grocery_list = {}
-  items.each do |item|
+
+  grocery_items.each do |item|
     grocery_list[item] = 1
   end
-  puts grocery_list
+  grocery_list
 end
 
-def add_list(item, quantity)
-  grocery_list[item] = []
+def add_items(list, item, quantity= 1)
 
+  list[item] = quantity
+  list
 end
 
+def remove_items(list, item)
 
+  if list.has_key?(item)
+    list.delete(item)
+    list
+  end
+end
 
+def update_qty(list, item, quantity)
+  if list.has_key?(item)
+    list[item] = quantity
+    list
+  end
+end
 
+def print_list(list)
+  list.each do |items, qty|
+    puts "#{items}:#{qty}"
+  end
+end
 
-
+list = create_list("Lemonade Tomatoes")
+update_qty(list, "Lemonade", 2)
+update_qty(list, "Tomatoes", 3)
+add_items(list, "Onions")
+add_items(list, "Ice Cream", 4)
+remove_items(list, "Lemonade")
+update_qty(list, "Ice Cream", 1)
+print_list(list)
 
 
 
