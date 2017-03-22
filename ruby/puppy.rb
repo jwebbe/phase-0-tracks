8ace24114dbd6ec62d29e612aef3c2a980612149
Jay Webbe <jwebbe@me.com>
2017-03-21 23:28:25 -0400

    complete 6.1 assignment with psdocode and class of puppy.

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+# --- 6.1 Introduction to Classes ---
+
+#  RELEASE 0: Design a Class
+
+# PUPPY ------------------------
+# Beagle
+
+# CHARACTERISTICS ----------------
+# Sensitivity: very high
+# Friendly: yes
+# Tolerates Being Alone: no
+# Intensity: intense
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Hug
+# Scamper
+
+# RELEASE 1: Release 1: Declare Your Class and Create an Instance in IRB
+
+class Puppy
+
+end
+
